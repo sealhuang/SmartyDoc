da4cd6d851367e9efe2427c419d19672e2e7d096
Lijie Huang <huanglijie@outlook.com>
2019-12-27 18:15:20 +0800

    update

--- smartydoc/ipynb2html.py
+++ smartydoc/ipynb2html.py
@@ -11,6 +11,8 @@ class IpyNBHTMLParser(HTMLParser):
         self.tag_stack = []
         self.toc_tree = OrderedDict()
         self._cur_toc_pos = OrderedDict()
+        self._article_intro = OrderedDict()
+        self._article_intro_flag = False
         # heading index vars
         self._h2_idx = 0
         self._h3_idx = 0
@@ -72,6 +74,10 @@ class IpyNBHTMLParser(HTMLParser):
                         h_content.append("%s='%s'"%(key, tag_content[key]))
                 self._h4_idx += 1
                 self.out_html += '<' + ' '.join(h_content) + '>' + '.'.join([str(self._h2_idx), str(self._h3_idx), str(self._h4_idx)]) + ' '
+            elif tag=='article_intro':
+                self.tag_stack.append(tag)
+                self._article_intro_flag = True
+                self._article_intro[self._h2_idx] = ''
             else:
                 self.tag_stack.append(tag)
                 h_content = [tag]
@@ -128,12 +134,17 @@ class IpyNBHTMLParser(HTMLParser):
                 self.tag_stack.pop(-1)
                 if self._anchor_link:
                     self._anchor_link = False
+            elif tag=='article_intro':
+                self.tag_stack.pop(-1)
+                self._article_intro_flag = False
+                #print(self._article_intro)
             else:
                 self.out_html += '</' + tag + '>\n'
                 self.tag_stack.pop(-1)
                 if tag=='h1' or tag=='h2':
                     self.tag_stack.append('div')
                     self.out_html += '<div>\n'
+
         elif tag=='body':
             while self.tag_stack[-1] in ['article', 'article_content',
                                          'div', 'section']:
@@ -146,15 +157,21 @@ class IpyNBHTMLParser(HTMLParser):
             print('Unrecognized tag %s'%(tag))
 
     def handle_data(self, data):
-        if not self._anchor_link:
+        if (not self._anchor_link) and (not self._article_intro_flag):
             self.out_html += data
+        if self._article_intro_flag:
+            self._article_intro[self._h2_idx] += data
 
     def handle_charref(self, name):
         pass
 
     def export2html(self, html_file, toc_level=1,
                     include_article_summary=False):
-        """toc_level: 0 for no toc"""
+        """toc_level: 0 for no toc.
+        include_article_summary: 'toc', 'intro', or False.
+        
+        """
+        h2_idx = 1
         with open(html_file, 'w') as f:
             split_content = self.out_html.split('\n')
             cover_start_flag = False
@@ -164,9 +181,10 @@ class IpyNBHTMLParser(HTMLParser):
                 
                 # add article summary
                 if line.startswith('<article id=') and toc_over_flag:
-                    if include_article_summary:
+                    # add article toc
+                    if include_article_summary == 'toc':
                         h2_id = line[13:-2]
-                        f.write('<article class="article-summary">\n')
+                        f.write('<article class="article-toc">\n')
                         f.write('<h2>%s</h2>\n'%(h2_id))
                         h3s = self.toc_tree[h2_id]
                         if h3s:
@@ -177,13 +195,26 @@ class IpyNBHTMLParser(HTMLParser):
                                 f.write('<li>%s</li>\n'%(h3))
                             f.write('</ul>\n')
                         f.write('</article>\n')
+ 
+                    # add article introduction
+                    if include_article_summary == 'intro':
+                        h2_id = line[13:-2]
+                        f.write('<article class="article-summary">\n')
+                        f.write('<h2>%s</h2>\n'%(h2_id))
+                        if h2_idx in self._article_intro:
+                            f.write('<article_intro>')
+                            f.write(self._article_intro[h2_idx]+'\n')
+                            f.write('</article_intro>\n')
+                        f.write('</article>\n')
+                        h2_idx += 1
+
                 if line.startswith('<h2 id=') and include_article_summary:
                     continue
                 if line[-5:]=='</h2>' and include_article_summary:
                     continue
                 
                 f.write(line+'\n')
-                
+ 
                 if line=="<article id='cover'>":
                     cover_start_flag = True
                 if line=='</article>' and cover_start_flag:
